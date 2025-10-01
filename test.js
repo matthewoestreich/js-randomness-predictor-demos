65395753b669751d536902204c3a57a83e4e9b83
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-01 12:40:04 -0500

    update

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-function main() {
-  const jsText = "console.log(1);";
-  const jsBlob = new Blob([jsText], { type: "text/javascript" });
-  const mainScriptUrlOrBlob = URL.createObjectURL(jsBlob);
-  console.log(mainScriptUrlOrBlob.toString());
-}
-main();
+
