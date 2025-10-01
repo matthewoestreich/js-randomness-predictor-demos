77c3465317374622efe14061d7fe9bd52f613dcc
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-01 12:43:47 -0500

    update

--- test.js
+++ test.js
@@ -1,1 +1,26 @@
+let e = undefined;
+callPOC();
 
+function callPOC() {
+  try {
+    console.log("callPOC");
+    if (emscriptenPOC != undefined) {
+      e = emscriptenPOC;
+    }
+    if (!e || e == undefined) {
+      console.log("e not found");
+      setTimeout(() => callPOC(), 1000);
+    } else {
+      e = emscriptenPOC;
+      e(Module).then((moduleInstance) => console.log("emscriptenPOC() has been called!", { moduleInstance }));
+    }
+  } catch (err) {
+    if (!e || e == undefined) {
+      console.log("e not found");
+      setTimeout(() => callPOC(), 1000);
+    } else {
+      e = emscriptenPOC;
+      e(Module).then((moduleInstance) => console.log("emscriptenPOC() has been called!", { moduleInstance }));
+    }
+  }
+}
