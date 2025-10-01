0fd4fcc2a81d8ce69cba06b2ca7e2ac195f1216b
Matt Oestreich <matthewpoestreich@gmail.com>
2025-09-30 20:09:36 -0500

    update

--- build.js
+++ build.js
@@ -5,9 +5,14 @@ const indexhtmlSrc = path.resolve(__dirname, "./index.html");
 const destDir = path.resolve(__dirname, "./docs");
 const indexhtmlDest = path.resolve(destDir, "index.html");
 
+// JUST TESTING THIS
+const emscSrc = path.resolve(__dirname, "./emscripten.html");
+const emscDest = path.resolve(destDir, "./emscripten.html");
+
 try {
   fs.mkdirSync(destDir);
   fs.copyFileSync(indexhtmlSrc, indexhtmlDest);
+  fs.copyFileSync(emscSrc, emscDest);
 } catch (e) {
   console.error(e);
   process.exit(1);
