585ae25dc9bd4fdd9659d99e7278e780c42d8068
Matt Oestreich <matthewpoestreich@gmail.com>
2025-10-01 17:57:18 -0500

    update

--- react/vite/vite.config.js
+++ react/vite/vite.config.js
@@ -1,43 +1,39 @@
-import nodePath from "node:path";
+import nodepath from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const root = nodepath.resolve(__dirname, "../../react/vite");
+
+// Server under folder /js-randomness-predictor-demos/ while on github pages.
+const base = process.env.BUILD_FOR === "prod" ? "/js-randomness-predictor-demos/" : "";
+
+// So we can go to "http://localhost:5173/docs/vanilla-js.html", etc...
+// As the 'nodepath.resolve' suggest, this path is relative to the "root" option.
+const publicDir = nodepath.resolve(root, "../../");
+
+console.log({ base, root, publicDir });
+
 export default defineConfig({
-  root: nodePath.resolve(__dirname, "../../react/vite"),
-  base: process.env.BUILD_FOR === "prod" ? "/js-randomness-predictor-demos/" : "/",
+  root,
+  base,
+  publicDir,
   build: {
     outDir: "../../docs",
     rollupOptions: {
       input: {
-        main: nodePath.resolve(__dirname, "./react-vite.html"),
+        main: nodepath.resolve(__dirname, "./react-vite.html"),
       },
     },
   },
-  plugins: [
-    react(),
-    //{
-    //  name: "configure-response-headers",
-    //  configureServer: (server) => {
-    //    server.middlewares.use((_req, res, next) => {
-    //      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-    //      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-    //      next();
-    //    });
-    //  },
-    //},
-  ],
+  plugins: [react()],
   preview: {
-    //headers: {
-    //  "Cross-Origin-Opener-Policy": "same-origin",
-    //  "Cross-Origin-Embedder-Policy": "require-corp",
-    //},
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
     open: "react-vite.html",
   },
   server: {
-    //headers: {
-    //  "Cross-Origin-Opener-Policy": "same-origin",
-    //  "Cross-Origin-Embedder-Policy": "require-corp",
-    //},
     open: "react-vite.html",
   },
   define: {
